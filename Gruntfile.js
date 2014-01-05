5b8a7ab3e8be4f274bcc655cc2fcb99553146437
Matt Styles <matt@veryfizzyjelly.com>
2014-01-05 22:46:07 +0000

    Bug fix for tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function(grunt) {
 
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
 	// plugin's task(s), then test the result.
-	grunt.registerTask('test', ['clean', 'hogan_static', 'nodeunit']);
+	grunt.registerTask('test', ['hogan_static', 'nodeunit', 'clean']);
 
 	// By default, lint and run all tests.
 	//grunt.registerTask('default', ['jshint', 'test']);
